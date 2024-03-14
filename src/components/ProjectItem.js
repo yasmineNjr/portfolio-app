3bc900daec1b7f6f51ec9880a931b2dae5f1adcb
yamineHjr <yasmine.alnajar@gmail.com>
2024-03-14 17:38:45 +0200

    agin check on mobile

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -27,9 +27,9 @@ const ProjectItem = (props) => {
                                 <p>{props.project.description}</p> 
                                 {
                                     props.project.url !== '' && 
-                                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: '0', padding: '0'}}>
+                                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '0.5rem', padding: '0'}}>
                                         <FaLink color='#8e44ad' size='1.2rem' style={{marginRight: '0.5rem'}}/>
-                                        <p >
+                                        <p style={{padding: '0'}}>
                                             <a href={props.project.url} target='_blank' rel='noreferrer'>{props.project.urlText}</a>
                                             {/* <Link to='/Bio'>{props.project.url}</Link> */}
                                         </p>
