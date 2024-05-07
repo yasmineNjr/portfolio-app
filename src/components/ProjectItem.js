94b92e013414481064bff9626b517127586e6965
yamineHjr <yasmine.alnajar@gmail.com>
2024-05-07 19:12:14 +0300

    add ScheduleEase video url

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,16 +1,13 @@
 import classes from './ProjectItem.module.css';
-// import {Link} from 'react-router-dom';
 import Card from '../components/layouts/Card';
 import { FaLink } from "react-icons/fa6";
+import { BiVideo } from "react-icons/bi";
 
 const ProjectItem = (props) => {
 
     return  <section className={classes.section}>
-                <Card height='90%'>
+                <Card height='95%'>
                     <div>
-                        {/* <div  style={{ textAlign: 'center', color: '#EED3D9' }}>
-                            <strong>{props.index} of {props.count}</strong>
-                        </div> */}
                         <div className={classes.maindiv}>
                             <div className={classes.imgdiv}>
                                 <img src={props.project.image} alt='logo' className={classes.img}/>
@@ -27,14 +24,21 @@ const ProjectItem = (props) => {
                                 <p>{props.project.description}</p> 
                                 {
                                     props.project.url !== '' && 
-                                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '0.5rem', padding: '0'}}>
+                                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
                                         <FaLink color='#8e44ad' size='1.2rem' style={{marginRight: '0.5rem'}}/>
                                         <p style={{padding: '0'}}>
                                             <a href={props.project.url} target='_blank' rel='noreferrer'>{props.project.urlText}</a>
-                                            {/* <Link to='/Bio'>{props.project.url}</Link> */}
                                         </p>
                                     </div>
-                                        
+                                }
+                                {
+                                    props.project.video !== '' &&
+                                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '0.5rem', padding: '0'}}>
+                                        <BiVideo color='#8e44ad' size='1.2rem' style={{marginRight: '0.5rem'}}/>
+                                        <p style={{padding: '0'}}>
+                                            <a href={props.project.video} target='_blank' rel='noreferrer'>{props.project.videoText}</a>
+                                        </p>
+                                    </div>
                                 }
                             </div>
                         </div>
--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,9 @@ const PROJECTS = [
         technics: ['NextJs', 'MongoDB', ],
         description: 'An e-commerce platform developed with Next.js and MongoDB. The store showcases a comprehensive range of products, providing detailed information for each item.  It empowers marketers to seamlessly add products to the shopping cart. The system eﬃciently manages orders contributed by each marketer within the team, monitors order statuses, calculates individual marketer proﬁts, includes an upgradeability feature for each marketer, and oﬀers statistical charts illustrating the growth in customers, marketers, and sales. ',
         url: 'https://forever-website.vercel.app/',
-        urlText: 'AKTStore website link'
+        urlText: 'AKTStore website link',
+        video: '',
+        videoText: '',
     },
     {
         id: '2',
@@ -15,7 +17,9 @@ const PROJECTS = [
         technics: ['ReactJs', 'LocalStorage', 'Socket.io'],
         description: 'A meeting scheduler app developed using React and Socket.io. Any user can define a meeting with possible dates and access meeting information when necessary. The application generates a code and associates this code with the relevant meeting. Any user can access meeting information via the shared code and see the most suitable times for everyone on the calendar. Any user can register to the meeting using the shared code and name and determine the most suitable time slots among possible dates. The user who creates the meeting chooses the best time for everyone by looking at calendar, Finally, the date specified for the meeting can be added to Google calendar. ',
         url: 'https://schedule-a-meeting-client.onrender.com ',
-        urlText: 'SchedulerEase website link'
+        urlText: 'SchedulerEase website link',
+        video: 'https://drive.google.com/file/d/1-a4kx_rhOQcx6XZHG67CvYzCnnowd3RD/view?usp=sharing',
+        videoText: 'SchedulerEase videa link',
     },
     {
         id: '3',
@@ -23,8 +27,10 @@ const PROJECTS = [
         title : 'Emlak ',
         technics: ['ASP.Net', 'Sql Server DB', 'MVC'],
         description: 'A website created using ASP.NET with C# language and a SQL Server database. It showcases real estate and provides a comprehensive suite of services for sellers, buyers of residential properties, and tenants. The platform features an intuitive and simpliﬁed interface, enabling users to easily furnish detailed property descriptions and upload a set of pictures for each listing. Additionally, it streamlines communication between sellers and buyers. ',
-        url: 'https://drive.google.com/file/d/1HMbNDL8qdhykz9E9yeCZcUugpfiEThnR/view?usp=sharing',
-        urlText: 'Emlak video link'
+        url: '',
+        urlText: '',
+        video: 'https://drive.google.com/file/d/1HMbNDL8qdhykz9E9yeCZcUugpfiEThnR/view?usp=sharing',
+        videoText: 'Emlak video link',
     },
     {
         id: '4',
@@ -32,8 +38,10 @@ const PROJECTS = [
         title : 'InstiSys ',
         technics: ['WPF', 'Sql Server DB', 'Entity Frameorkk'],
         description: 'A desktop application developed using C#.NET with a SQL Server database, designed to manage educational courses and their details. It facilitates student registration and enrollment in the appropriate courses, saves feerelated information, and allows users to inquire about all outstanding fees up to a speciﬁc date. The application also handles the calculation of income, expenses, and the deserved fees of individual students. Additionally, it maintains employee data and calculates their monthly salaries. ',
-        url: 'https://drive.google.com/file/d/1Uc2k6l_-julyfKQ1VXa1y6B7MXu60kpK/view?usp=sharing',
-        urlText: 'InstiSys video link'
+        url: '',
+        urlText: '',
+        video: 'https://drive.google.com/file/d/1Uc2k6l_-julyfKQ1VXa1y6B7MXu60kpK/view?usp=sharing',
+        videoText: 'InstiSys video link',
     },
 ]
 
